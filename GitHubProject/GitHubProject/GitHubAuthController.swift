32b441527c57c0708dbe26cf52d6874b0c924d61
David <davidporter33@icloud.com>
2017-04-03 13:37:43 -0700

    Corner radius added to print token button and login button.

--- GitHubProject/GitHubProject/GitHubAuthController.swift
+++ GitHubProject/GitHubProject/GitHubAuthController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class GitHubAuthController: UIViewController {
+    @IBOutlet weak var printTokenOutlet: UIButton!
+    @IBOutlet weak var loginOutlet: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        printTokenOutlet.layer.cornerRadius = 10
+        loginOutlet.layer.cornerRadius = 10
 
         // Do any additional setup after loading the view.
     }
